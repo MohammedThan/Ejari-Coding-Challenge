3d4e395ccbf17cc5919930a86c1120d52b1b985e
Mohammed Al-Thunayan <mohammedth.912@gmail.com>
2024-10-24 15:08:33 +0100

    change local database name

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/ejari";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/ejari-books";
 
 export const PORT = process.env.PORT || 5000;
