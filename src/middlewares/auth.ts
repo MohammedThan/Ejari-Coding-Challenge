f4134c05b67227525afb8655c05c7e91a61709e1
Mohammed Al-Thunayan <mohammedth.912@gmail.com>
2024-10-26 21:59:35 +0200

    delete unneeded logs

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -9,19 +9,16 @@ const basicAuth = (req: Request, res: Response, next: NextFunction) => {
     return;
   }
 
-  console.log("authHeader", authHeader);
   // Extract the Base64 encoded string and decode it
   const base64Credentials = authHeader.split(" ")[1];
-  console.log("base64Credentials", base64Credentials);
+
 
   const credentials = Buffer.from(base64Credentials, "base64").toString(
     "utf-8"
   );
-  console.log("credentials", credentials);
 
   const [username, password] = credentials.split(":");
-  console.log("username", username);
-  console.log("password", password);
+
 
   // Check if the username and password are correct
   if (username === BASIC_AUTH_USERNAME && password === BASIC_AUTH_PASSWORD) {
